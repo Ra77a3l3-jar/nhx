87fa249d15a0ab0057a9ef62fd63bf85afbd0d32
Ra77a3l3-jar <raffaelemeo77@gmail.com>
2026-08-23 09:43:56 +0200

    forest: 0-unstable-2026-08-22 -> 0-unstable-2026-08-23

--- pkgs/helixPlugins/forest.nix
+++ pkgs/helixPlugins/forest.nix
@@ -8,15 +8,15 @@
 }:
 buildHelixPlugin (finalAttrs: {
   pname = "forest.hx";
-  version = "0-unstable-2026-08-22";
+  version = "0-unstable-2026-08-23";
   cogName = "forest";
   updateVersion = "branch";
 
   src = fetchFromGitHub {
     owner = "Ra77a3l3-jar";
     repo = finalAttrs.pname;
-    rev = "7a67b05c6919c9a670a5591fe1e21832d1edcdda";
-    hash = "sha256-0m8Az1B4mLzg9XFkYe3HQtpSiDkGz3c5N+8lFqZ95Ts=";
+    rev = "9f26b361179f0f4d466d8cf218677c57b574ea71";
+    hash = "sha256-rro99PUoAcM1McWihEuuu8B1MRSX8ZAqS2z5BVPJPzY=";
   };
 
   pluginDependencies = [
